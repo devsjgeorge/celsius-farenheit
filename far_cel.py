cada227e9b664a5972d79af8b303cba8c78da6c1
Sajini Joby <dev.sjgeorge@gmail.com>
2021-07-21 06:00:53 -0400

    Updated the flow and polished the interactivity

--- far_cel.py
+++ far_cel.py
@@ -4,28 +4,28 @@
 Created on Tue Mar 23 10:36:36 2021
 
 @author: sajinijoby
+
+This is a sample program to interactively convert C > F and F > C.
+
 """
 
-# python program for temperature in degree celsius and convert into degree Farenheit.
-# change this value into degree farenheit
 
-celsius = 37.5
-#calculate farenheit
-farenheit = (celsius * 1.8) + 32
-print(farenheit)
-
-#python program for convert farenheit to celsius
-fareheit =98
-#calculate celsius
-celsius = (farenheit - 32) * 5 / 9
-print(celsius)
+print("Welcome to Temperature Convertor!")
 
 while True:
-   cel = input('what is the degree in celsius')
-   far = (float(cel) * 1.8) + 32
-   print(far)
-   
-   far = input('what is the degree in farenheit')
-   cel = (float(far) - 32) * 5 / 9
-   print(cel)
-   break
+   # print("Please choose, c - convert to celsius; f - convert to farenheit; q - quit")
+   user_choice = input("Choose, c - convert to celsius; f - convert to farenheit; q - quit # ")
+   if (user_choice == 'q'):
+       break;
+   elif (user_choice == 'c'):
+       far = input('What is the degree in farenheit?: ')
+       cel = round((float(far) - 32) * 5 / 9,2)
+       print("Converted temperature is: "+str(cel)+" C")
+   elif (user_choice == 'f'):
+       cel = input('What is the degree in celsius?: ')
+       far = round((float(cel) * 1.8) + 32,2)
+       print("Converted temperature is: "+str(far)+" F")
+   else :
+       print("I dont understand your choice; Please try again!")
+       
+   # break
